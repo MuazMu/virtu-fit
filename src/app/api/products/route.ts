897bb42de9d267cad1bd4c92ebbd90e5ff168245
Muaz Abdulkhafur <muwi1882@gmail.com>
2025-05-31 17:33:19 +0300

    fixed

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,9 +2,10 @@ export async function GET() {
   return new Response(
     JSON.stringify({
       products: [
-        { id: '1', title: 'Jacket', image: '/mock-clothes/jacket.png', price: '$99.00' },
-        { id: '2', title: 'Dress', image: '/mock-clothes/dress.png', price: '$79.00' },
-        { id: '3', title: 'Tee', image: '/mock-clothes/tee.png', price: '$29.00' },
+        { id: '1', title: 'Pants 2', image: '/mock-clothes/pants2.jpg', price: '$99.00' },
+        { id: '2', title: 'Pants 1', image: '/mock-clothes/pants1.jpg', price: '$79.00' },
+        { id: '3', title: 'Shirt 1', image: '/mock-clothes/shirt1.jpg', price: '$29.00' },
+        { id: '4', title: 'T-shirt 1', image: '/mock-clothes/tshirt1.jpg', price: '$19.00' },
       ],
     }),
     {
